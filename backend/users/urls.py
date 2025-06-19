64dc0e956c49532b9a3e3234b0029a99efa75f42
Bramtheking <141567417+Bramtheking@users.noreply.github.com>
2025-06-19 19:44:22 +0300

    Update urls.py

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('health/', views.health_check, name='health_check'),
     path('profile/', views.UserProfileView.as_view(), name='user_profile'),
-    path('sync/', views.UserCreateView.as_view(), name='user_sync'),
+    path('sync/', views.UserSyncView.as_view(), name='user_sync'),
     path('logout/', views.logout_view, name='logout'),
 ]
